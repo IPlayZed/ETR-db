3c9a827f31b2e6ac0af600df29dc0b6ff17b052c
IPlayZed <pauljouser@gmail.com>
2020-11-11 17:57:10 +0100

    created views module for main app and added skeleton

--- etr/etr_app/views.py
+++ etr/etr_app/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import request, response, Http404,HttpResponse
+from django.shortcuts import render, get_object_or_404der
 
 # Create your views here.
+def homepage(request):
+    pass
